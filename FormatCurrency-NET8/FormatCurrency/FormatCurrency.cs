c0a15d4439ac5006daf05a9a3c1cbc6198ca58b5
Alexandre R <alexrealinho@gmail.com>
2026-08-27 10:10:12 +0100

    Add files via upload

---
+++ FormatCurrency-NET8/FormatCurrency/FormatCurrency.cs
@@ -0,0 +1,338 @@
+﻿using OutSystems.ExternalLibraries.SDK;
+using System.Globalization;
+using System.Text;
+
+namespace OutSystems.FormatCurrency
+{
+    public class FormatCurrency : IFormatCurrency
+    {
+
+        //chinese support functions
+        private string[] chineseNumbers = new string[] { "〇", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "百", "千" }; // 0..9, 10, 100, 1000
+        private string[] chineseNumbersfinancial = new string[] { "零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖", "拾", "佰", "仟" }; // 0..9, 10, 100, 1000
+        private string[] chineseBigNums = new string[] { "万", "亿", "兆", "京" };  // 10^4, 10^8, 10^12, 10^16
+        private string[] chineseBigNumsfinancial = new string[] { "萬", "亿", "兆", "京" };  // 10^4, 10^8, 10^12, 10^16
+        private string ChineseSmallNumberConvert(long number, string appendedNumber, string[] numbers)
+        {
+            if (number >= 10000)
+            {
+                throw (new ArgumentOutOfRangeException("n", "number must be less than 10000"));
+            }
+
+            StringBuilder b = new StringBuilder();
+
+            int n = 1000;
+            int idx = 12;
+
+            do
+            {
+                if (number >= n)                                // bug fix: changed from > (thanks to Alexandre Realinho)
+                {
+
+                    b.Append(numbers[number / n]);
+
+                    if (n >= 10)
+                    {
+                        b.Append(numbers[idx]);
+                    }
+
+                    number %= n;
+                }
+                else if (number / n == 0 && b.Length != 0 && number != 0 && b.ToString(b.Length - 1, 1) != chineseNumbersfinancial[0])
+                { // gap 0 for small numbers
+                    b.Append(chineseNumbersfinancial[0]);
+                }
+                else if (n == 1000 && number / n == 0 && appendedNumber.Length != 0 && number != 0 && appendedNumber[appendedNumber.Length - 1].ToString() != chineseNumbersfinancial[0])
+                { // gap 0 for the big numbers
+                    b.Append(chineseNumbersfinancial[0]);
+                }
+
+                n /= 10; // 100 / 10 = 10
+                idx--; // 11 - 1 = 10
+            }
+            while (n > 0);
+
+            return b.ToString();
+        }
+        private string ChineseNumberConvert(long _number, string[] numbers, string[] bigNumbers)
+        {
+            StringBuilder b = new StringBuilder();
+            long baseNum = 10000000000000000;       // 10^16
+
+            if (_number == 0)
+            {
+                b.Append(numbers[0]);
+            }
+            else
+            {
+                int baseIdx = 3;
+
+                do
+                {
+                    long n = _number / baseNum;
+                    if (n > 0)
+                    {
+                        b.Append(ChineseSmallNumberConvert(n, b.ToString(), numbers));
+                        if (baseIdx >= 0)
+                        {
+
+                            b.Append(bigNumbers[baseIdx]);
+
+                        }
+                    }
+
+                    _number %= baseNum;
+                    baseNum /= 10000;
+                    baseIdx--;
+                }
+                while (baseNum > 0);
+            }
+            if (b.Length >= 2 && ((b.ToString()).Substring(0, 2) == String.Concat(numbers[1], numbers[10])))
+            {                   //bug fix: remove 一 from the first 十 of the string
+                return b.Remove(0, 1).ToString();
+            }
+            return b.ToString();
+        }
+
+        //end chinese support functions
+
+        public void GetDecimalFromLocaleDecimalString(string InputLocalelDecimalString, string Locale, string Currency, out bool IsValidDecimal, out int ErrorMessageCode, out string ErrorMessage, out decimal Decimal)
+        {
+            IsValidDecimal = true;
+            ErrorMessageCode = 0;
+            ErrorMessage = "";
+            Decimal = 0.0m;
+            CultureInfo culture;
+            // Return if string is empty
+            if (String.IsNullOrEmpty(InputLocalelDecimalString))
+            {
+                ErrorMessage = "String Empty";
+                ErrorMessageCode = 1;
+                return;
+            }
+            // Instantiate CultureInfo object for the user's locale
+            try
+            {
+                culture = CultureInfo.GetCultureInfo(Locale);
+            }
+            catch (CultureNotFoundException e)
+            {
+                ErrorMessage = "Locale Invalid/Not Provided";
+                ErrorMessageCode = 2;
+                return;
+            }
+            // Convert user input from a string to a number
+            try
+            {
+                NumberFormatInfo numberFormatInfo = (NumberFormatInfo)culture.NumberFormat.Clone();
+                int count = 0;
+                foreach (string item in numberFormatInfo.NativeDigits)
+                {
+                    if (InputLocalelDecimalString.IndexOf(item) > -1)
+                    {
+                        string scount = count.ToString();
+                        InputLocalelDecimalString = InputLocalelDecimalString.Replace(item, scount);
+                    }
+                    count++;
+                }
+                if (!Currency.Equals(""))
+                {
+                    numberFormatInfo.CurrencySymbol = Currency;
+
+                }
+                Decimal = Decimal.Parse(InputLocalelDecimalString, NumberStyles.Currency, numberFormatInfo);
+
+            }
+            catch (FormatException e)
+            {
+                ErrorMessage = "FormatException [" + e + "]";
+                ErrorMessageCode = 3;
+                return;
+            }
+            catch (Exception e)
+            {
+                ErrorMessage = "Other Errors [" + e + "]";
+                ErrorMessageCode = 4;
+                return;
+            }
+        }
+
+        public void GetCurrencyFormattedByLocale(string Locale, decimal Decimal, bool HasCurrency, string Currency, bool UseNativeDigits, bool UseChineseExtendedNumbers, bool UseFinancialChinese, out string FormattedText)
+        {
+
+            Dictionary<int, String> patterns = new Dictionary<int, String>();
+            string[] patternStrings = { "($n)", "-$n", "$-n", "$n-", "(n$)",
+                                 "-n$", "n-$", "n$-", "-n $", "-$ n",
+                                 "n $-", "$ n-", "$ -n", "n- $", "($ n)",
+                                 "(n $)" };
+            for (int ctr = patternStrings.GetLowerBound(0);
+                 ctr <= patternStrings.GetUpperBound(0); ctr++)
+                patterns.Add(ctr, patternStrings[ctr]);
+
+            Dictionary<int, String> patternspositive = new Dictionary<int, String>();
+            string[] patternStrings2 = { "$n", "n$", "$ n", "n $" };
+            for (int ctr = patternStrings2.GetLowerBound(0);
+                 ctr <= patternStrings2.GetUpperBound(0); ctr++)
+                patternspositive.Add(ctr, patternStrings2[ctr]);
+
+            FormattedText = "";
+            CultureInfo ci;
+            try
+            {
+                ci = CultureInfo.GetCultureInfo(Locale);
+            }
+            catch (Exception e)
+            {
+                ci = CultureInfo.GetCultureInfo("");
+            }
+            NumberFormatInfo numberFormatInfo = (NumberFormatInfo)ci.NumberFormat.Clone();
+            if (!Currency.Equals(""))
+            {
+                numberFormatInfo.CurrencySymbol = Currency;
+
+            }
+            if (!HasCurrency)
+            {
+                numberFormatInfo.CurrencySymbol = "";
+
+            }
+
+            if (UseChineseExtendedNumbers && UseNativeDigits && Locale.IndexOf("zh") == 0)
+            {
+                long wholePart = (long)Math.Truncate(Math.Abs(Decimal));
+                string formattedChin = "";
+                if (("" + Math.Abs(Decimal)).IndexOf(".") > 0)
+                {
+
+                    int lengthDecimal = ("" + Math.Abs(Decimal)).Substring(("" + Math.Abs(Decimal)).IndexOf(".")).Length - 1;
+                    long decimalPart = (long)((decimal)Math.Pow(10, lengthDecimal) * (+Math.Abs(Decimal) - Math.Truncate(+Math.Abs(Decimal))));
+                    string decimalPartString = decimalPart.ToString();
+                    numberFormatInfo.CurrencyDecimalSeparator = "点";
+                    if (UseFinancialChinese)
+                    {
+                        int count = 0;
+                        foreach (string item in chineseNumbersfinancial)
+                        {
+                            string scount = count.ToString();
+                            decimalPartString = decimalPartString.Replace(scount, item);
+                            count++;
+                            if (count > 9)
+                            {
+                                break;
+                            }
+                        }
+                        formattedChin = ChineseNumberConvert(wholePart, chineseNumbersfinancial, chineseBigNumsfinancial) +
+                                    numberFormatInfo.CurrencyDecimalSeparator +
+                                    decimalPartString;
+                    }
+                    else
+                    {
+                        int count = 0;
+                        foreach (string item in chineseNumbers)
+                        {
+                            string scount = count.ToString();
+                            decimalPartString = decimalPartString.Replace(scount, item);
+                            count++;
+                            if (count > 9)
+                            {
+                                break;
+                            }
+                        }
+                        formattedChin = ChineseNumberConvert(wholePart, chineseNumbers, chineseBigNums) +
+                                    numberFormatInfo.CurrencyDecimalSeparator +
+                                    decimalPartString;
+                    }
+
+
+                }
+                else
+                {
+                    if (UseFinancialChinese)
+                    {
+                        formattedChin = ChineseNumberConvert(wholePart, chineseNumbersfinancial, chineseBigNumsfinancial);
+                    }
+                    else
+                    {
+                        formattedChin = ChineseNumberConvert(wholePart, chineseNumbers, chineseBigNums);
+                    }
+                }
+                if (Decimal >= 0)
+                {
+                    FormattedText = patternspositive[numberFormatInfo.CurrencyPositivePattern].Replace("n", formattedChin).Replace("$", numberFormatInfo.CurrencySymbol);
+                }
+                else
+                {
+                    FormattedText = patterns[numberFormatInfo.CurrencyNegativePattern].Replace("n", formattedChin).Replace("-", numberFormatInfo.NegativeSign).Replace("$", numberFormatInfo.CurrencySymbol);
+                }
+
+
+            }
+            else
+            {
+                FormattedText = Decimal.ToString("C", numberFormatInfo);
+                if (UseNativeDigits)
+                {
+                    int count = 0;
+                    foreach (string item in numberFormatInfo.NativeDigits)
+                    {
+                        string scount = count.ToString();
+                        FormattedText = FormattedText.Replace(scount, item);
+                        count++;
+                    }
+                }
+            }
+
+        }
+
+        public void GetLocales(out List<Locale> ListofLocals)
+        {
+            ListofLocals = new List<Locale>();
+
+           
+
+            foreach (CultureInfo cultureInfo in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                ListofLocals.Add(new Locale(cultureInfo));
+            }
+        }
+    }
+    [OSStructure]
+    public struct Locale
+    {
+        public string Name;
+        public string RFC4646;
+        public string CurrencyDecimalDigits;
+        public string CurrencyDecimalSeparator;
+        public string CurrencyGroupSeparator;
+        public string CurrencyGroupSizes;
+        public string CurrencyNegativePattern;
+        public string CurrencyPositivePattern;
+        public string NegativeSign;
+        public string CurrencySymbol;
+        public string NativeDigits;
+        
+        private string[] patternNegative = { "($n)", "-$n", "$-n", "$n-", "(n$)",
+                                 "-n$", "n-$", "n$-", "-n $", "-$ n",
+                                 "n $-", "$ n-", "$ -n", "n- $", "($ n)",
+                                 "(n $)" };
+        
+        private string[] patternsPositive = { "$n", "n$", "$ n", "n $" };
+        
+        public Locale(CultureInfo cultureInfo)
+        {
+            this.CurrencySymbol = cultureInfo.NumberFormat.CurrencySymbol;
+            this.Name = cultureInfo.DisplayName;
+            this.RFC4646 = cultureInfo.Name;
+            this.CurrencyDecimalDigits = cultureInfo.NumberFormat.CurrencyDecimalDigits.ToString();
+            this.CurrencyDecimalSeparator = cultureInfo.NumberFormat.CurrencyDecimalSeparator;
+            this.CurrencyGroupSeparator = cultureInfo.NumberFormat.CurrencyGroupSeparator;
+            //Error correction for pattern 16 
+            this.CurrencyNegativePattern = patternNegative[(cultureInfo.NumberFormat.CurrencyNegativePattern > 15) ? 2 : cultureInfo.NumberFormat.CurrencyNegativePattern];
+            this.CurrencyPositivePattern = patternsPositive[cultureInfo.NumberFormat.CurrencyPositivePattern];
+            this.CurrencyGroupSizes = "[" + string.Join(",", cultureInfo.NumberFormat.CurrencyGroupSizes) + "]";
+            this.NativeDigits = string.Join(",", cultureInfo.NumberFormat.NativeDigits);
+            this.NegativeSign = cultureInfo.NumberFormat.NegativeSign;
+        }
+
+    }
+}
---
+++ FormatCurrency-NET8/FormatCurrency/IFormatCurrency.cs
@@ -0,0 +1,103 @@
+﻿using OutSystems.ExternalLibraries.SDK;
+
+namespace OutSystems.FormatCurrency
+{
+    [OSInterface(Description = "Formats the decimal based on the locale provided and currency symbol from the locale or provided. Provides a list of the Locales available. Converts a Locale Decimal String (a decimal that was written in in a certain locale) into a valid Decimal.",IconResourceName = "OutSystems.FormatCurrency.resources.logo.png", Name = "FormatCurrency")]
+
+    public interface IFormatCurrency
+    {
+        /// <summary>
+        /// Formats the Decimal based on the Locale provided.
+        /// </summary>
+        /// <param name="Locale">The Locale of the format, see RFC4646 from GetLocales</param>
+        /// <param name="Decimal">The number to format</param>
+        /// <param name="HasCurrency">If the number has the currency symbol.</param>
+        /// <param name="Currency">The Currency to use, if &quot;&quot; it uses the default currency of the locale</param>
+        /// <param name="UseNativeDigits">If it uses the native symbols for 0-9.</param>
+        /// <param name="UseChineseExtendedNumbers">If the locale starts with "zh", and this is true, the native numbers will be using the chinese unit system instead of the latim or partial chinese numbers.</param>
+        /// <param name="UseFinancialChinese">If the locale starts with "zh", and this is true, it will use financial chinese numeration example:  零 "líng" instead of 〇, normally in financial applications 零 is used.</param>
+        /// <param name="FormattedText">The formatted text</param>
+        [OSAction(Description = "Formats the Decimal based on the Locale provided.", IconResourceName = "OutSystems.FormatCurrency.resources.logo.png")]
+        public void GetCurrencyFormattedByLocale(
+            [OSParameter(DataType = OSDataType.Text,    Description = "The Locale of the format, see RFC4646 from GetLocales")] 
+            string Locale,
+            
+            [OSParameter(DataType = OSDataType.Decimal, Description = "The number to format")] 
+            decimal Decimal,
+            
+            [OSParameter(DataType = OSDataType.Boolean, Description = "If the number has the currency symbol.")] 
+            bool HasCurrency,
+            
+            [OSParameter(DataType = OSDataType.Text,    Description = "The Currency to use, if \"\" it uses the default currency of the locale")]
+            string Currency,
+            
+            [OSParameter(DataType = OSDataType.Boolean, Description = "If it uses the native symbols for 0-9.")]
+            bool UseNativeDigits,
+            
+            [OSParameter(DataType = OSDataType.Boolean, Description = "If the locale starts with \"zh\", and this is true, the native numbers will be using the chinese unit system instead of the latim or partial chinese numbers.")]
+            bool UseChineseExtendedNumbers,
+            
+            [OSParameter(DataType = OSDataType.Boolean, Description = "If the locale starts with \"zh\", and this is true, it will use financial chinese numeration example:  零 \"líng\" instead of 〇, normally in financial applications 零 is used.")]
+            bool UseFinancialChinese,
+            
+            [OSParameter(DataType = OSDataType.Text,    Description = "The formatted text")]
+            out string FormattedText
+        );
+
+        //----------------------------------------------------------------
+        /// <summary>
+        /// Gets the C# supported Locales and their parametrizations.
+        /// </summary>
+        /// <param name="ListofLocals">The List of Locales</param>
+        [OSAction(Description = "Gets the C# supported Locales and their parametrizations.", IconResourceName = "OutSystems.FormatCurrency.resources.logo.png")]
+        public void GetLocales(
+            [OSParameter(DataType = OSDataType.InferredFromDotNetType,    Description = "The List of Locales")]
+            out List<Locale> ListofLocals
+        );
+
+        //----------------------------------------------------------------
+        /// <summary>
+		/// Converts a Locale Decimal String (a decimal that was written in in a certain locale) into a valid Decimal.
+		/// For now &quot;zh&quot; locale that uses the extended numbers and financial numbers will not be converted accordingly.
+		/// Error Message Codes and meanings:
+		/// 0 - &quot;&quot; - No Error
+		/// 1 - &quot;String Empty&quot; - The string was empty
+		/// 2 - &quot;Locale Invalid/Not Provided&quot; - No Locale was found
+		/// 3 - &quot;FormatException&quot; - an error in the format of the number
+		/// 4 - &quot;Other Errors [x]&quot; - other errors - log represented in x
+        /// 
+		/// </summary>
+		/// <param name="InputLocalelDecimalString">The String containing the Locale Decimal to convert into Decimal</param>
+		/// <param name="Locale">The Locale that the string was written on. (It will try both native and 0-9 numbers from the locale).</param>
+		/// <param name="Currency">The Currency to use, if &quot;&quot; it uses the default currency of the locale</param>
+        /// <param name="IsValidDecimal">If the converted Decimal is a valid one, if not then the Error Message will have the reason that the conversion failed.</param>
+		/// <param name="ErrorMessageCode">The code of the Error Message.</param>
+		/// <param name="ErrorMessage">Error Message in case the converted Decimal is not valid.</param>
+		/// <param name="Decimal">The Decimal converted, if the IsValidDecimal is notTrue, then this will be the default value 0.0.</param>
+		[OSAction(Description = "Converts a Locale Decimal String (a decimal that was written in in a certain locale) into a valid Decimal. " +
+                                "For now \"zh\" locale that uses the extended numbers and financial numbers will not be converted accordingly. " +
+                                "Error Message Codes and meanings: \n" +
+                                " 0 - &quot;&quot; - No Error \n" +
+                                " 1 - &quot;String Empty&quot; - The string was empty\n" +
+                                " 2 - &quot;Locale Invalid/Not Provided&quot; - No Locale was found\n" +
+                                " 3 - &quot;FormatException&quot; - an error in the format of the number\n" +
+                                " 4 - &quot;Other Errors [x]&quot; - other errors - log represented in x",
+            IconResourceName = "OutSystems.FormatCurrency.resources.logo.png")]
+        public void GetDecimalFromLocaleDecimalString(
+            [OSParameter(DataType = OSDataType.Text,    Description = "The String containing the Locale Decimal to convert into Decimal")]
+            string InputLocalelDecimalString,
+            [OSParameter(DataType = OSDataType.Text,    Description = "The Locale that the string was written on. (It will try both native and 0-9 numbers from the locale).")]
+            string Locale,
+            [OSParameter(DataType = OSDataType.Text,    Description = "The Currency to use, if &quot;&quot; it uses the default currency of the locale.")]
+            string Currency,
+            [OSParameter(DataType = OSDataType.Boolean, Description = "If the converted Decimal is a valid one, if not then the Error Message will have the reason that the conversion failed.")]
+            out bool IsValidDecimal,
+            [OSParameter(DataType = OSDataType.Integer, Description = "The code of the Error Message.")]
+            out int ErrorMessageCode,
+            [OSParameter(DataType = OSDataType.Text,    Description = "Error Message in case the converted Decimal is not valid.")]
+            out string ErrorMessage,
+            [OSParameter(DataType = OSDataType.Decimal, Description = "The Decimal converted, if the IsValidDecimal is notTrue, then this will be the default value 0.0.")]
+            out decimal Decimal);
+
+    }
+}
